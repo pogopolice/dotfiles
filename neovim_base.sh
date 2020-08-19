bb188399f89eee455471b77b9cd7817881cb3bc5
pogopolice <pogopolice@gmail.com>
2020-08-19 14:04:23 -0400

    add a script for setting up neovim

---
+++ neovim_base.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt install neovim
+
+# Add the config file and directories for neovim
+mkdir -p ~/.config/nvim
+touch .config/nvim/init.vim
+# let g:python3_host_prog = '/usr/bin/python3'
+
+# clipboard app
+sudo apt install xclip
+
+# python prereqs
+sudo apt install python3-neovim python3-pip
+
+# ruby prereqs
+sudo apt install ruby ruby-dev
+sudo apt install build-essential
+sudo gem install neovim
+
+# node prereqs
+sudo apt install nodejs npm
+sudo npm install -g neovim
+
+# plugin directory
+mkdir -p .local/share/nvim/site/pack/pogopolice/{start,opt}
