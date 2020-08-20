827de5e28506142542dda2e57bee42e60063da2d
pogopolice <pogopolice@gmail.com>
2020-08-20 12:00:38 -0400

    add a git ignore file

--- neovim_base.sh
+++ neovim_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install neovim
+sudo apt install -y neovim
 
 # Add the config file and directories for neovim
 mkdir -p ~/.config/nvim
@@ -8,19 +8,25 @@ touch .config/nvim/init.vim
 # let g:python3_host_prog = '/usr/bin/python3'
 
 # clipboard app
-sudo apt install xclip
+sudo apt install -y xclip
+
+# tmux app
+sudo apt install -y tmux
+
+# copy the tmux config into the home directory
+cp "$PWD/.tmux.conf" "$HOME"
 
 # python prereqs
-sudo apt install python3-neovim python3-pip
+sudo apt install -y python3-neovim python3-pip
 
 # ruby prereqs
-sudo apt install ruby ruby-dev
-sudo apt install build-essential
+sudo apt install -y ruby ruby-dev
+sudo apt install -y build-essential
 sudo gem install neovim
 
 # node prereqs
-sudo apt install nodejs npm
-sudo npm install -g neovim
+sudo apt install -y nodejs npm
+npm install -g neovim
 
 # plugin directory
 mkdir -p .local/share/nvim/site/pack/pogopolice/{start,opt}
