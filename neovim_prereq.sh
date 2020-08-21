dba47d300c447f3a8c21aa482ee081ab4466f4d9
pogopolice <pogopolice@gmail.com>
2020-08-21 11:25:39 -0400

    switch from setting up npm to yarn

--- neovim_prereq.sh
+++ neovim_prereq.sh
@@ -2,13 +2,26 @@
 
 
 #---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  add_repository
+#   DESCRIPTION:  Add the yarn repo to the sources list
+#-------------------------------------------------------------------------------
+
+add_repository ()
+{
+	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+	sudo apt update
+}	# ----------  end of function add_repository  ----------
+
+
+#---  FUNCTION  ----------------------------------------------------------------
 #          NAME:  install_packages
-#   DESCRIPTION:  install nodejs & npm
+#   DESCRIPTION:  install yarn
 #-------------------------------------------------------------------------------
 
 install_packages ()
 {
-	sudo apt install -y nodejs npm
+	sudo apt install -y yarn
 }	# ----------  end of function install_packages  ----------
 
 
@@ -19,22 +32,22 @@ install_packages ()
 
 global_directory ()
 {
-	mkdir -p "$HOME/.npm-global"
+	mkdir -p "$HOME/.config/yarn/global"
 	npm config set prefix "$HOME/.npm-global"
 }	# ----------  end of function global_directory  ----------
 
 
 #---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  npm_path
+#          NAME:  update_path
 #   DESCRIPTION:  add a local directory for installing global npm packages
 #-------------------------------------------------------------------------------
 
-npm_path ()
+update_path ()
 {
-	export PATH="$HOME/.npm-global/bin:$PATH"
+	export PATH="$HOME/.config/yarn/global:$PATH"
 	sed -i "/# export PATH=.*/a export PATH=$PATH" ~/.zshrc
 	
-}	# ----------  end of function npm_path  ----------
+}	# ----------  end of function update_path  ----------
 
 
 #---  FUNCTION  ----------------------------------------------------------------
@@ -58,9 +71,10 @@ print_message ()
 
 main ()
 {
+	add_repository
 	install_packages
-	global_directory
-	npm_path
+	#global_directory
+	update_path
 	print_message
 }	# ----------  end of function main  ----------
 
