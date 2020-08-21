cc4448c3a8c76ea175b3dca238d670151af4b051
pogopolice <pogopolice@gmail.com>
2020-08-21 13:15:29 -0400

    add the coc config step

--- coc_setup.sh
+++ coc_setup.sh
@@ -19,8 +19,57 @@
 
 set -o nounset                                  # Treat unset variables as an error
 
-pushd ~/.local/share/nvim/site/pack/pogopolice/start || return
-git clone https://github.com/neoclide/coc.nvim.git
-popd || return
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  install_plugin
+#   DESCRIPTION:  Install COC using vim's native package manager
+#-------------------------------------------------------------------------------
 
-cp ./coc.vim ~/.config/nvim/plug-config/coc.vim
+install_plugin ()
+{
+	mkdir -p ~/.local/share/nvim/site/pack/pogopolice/start
+	pushd ~/.local/share/nvim/site/pack/pogopolice/start || return
+	curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz|tar xzfv -
+	popd || return
+
+}	# ----------  end of function install_plugin  ----------
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  copy_config
+#   DESCRIPTION: copy the coc config file to the nvim plug conig directory
+#-------------------------------------------------------------------------------
+
+copy_config ()
+{
+	cp "$PWD/coc.vim" "$HOME/.config/nvim/plug-config/coc.vim"
+}	# ----------  end of function copy_config  ----------
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  source_config
+#   DESCRIPTION:  add a source call to the coc config in the neovim config
+#-------------------------------------------------------------------------------
+
+source_config ()
+{
+	echo "
+
+	\" COC config
+	source \$HOME/.config/nvim/plug-config/coc.vim
+	" | tee -a "$HOME/.config/nvim/init.vim"
+}	# ----------  end of function source_config  ----------
+
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  main
+#   DESCRIPTION: execute these functions in sequence
+#-------------------------------------------------------------------------------
+
+
+main ()
+{
+	install_plugin
+	copy_config
+	source_config
+}	# ----------  end of function main  ----------
+
+main
