f1cd0c402a8c647961bd34e5cfed19dc79036b1e
pogopolice <pogopolice@gmail.com>
2020-09-04 17:56:43 -0400

    add the nodejs repo and aws cli install

--- neovim_base.sh
+++ neovim_base.sh
@@ -24,12 +24,20 @@ sudo apt install -y build-essential
 sudo gem install neovim
 
 # nodejs
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y nodejs
+
+pushd "$HOME" || return
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo /bin/sh "$HOME/aws/install"
+rm -rf "$HOME/aws" "$HOME/awscliv2.zip"
+popd || return
 
 # yarn packages
 cp "$PWD/package.json" "$HOME"
 pushd "$HOME" || return
-yarn global add pkg.json --prefix ~/.local
+yarn global add package.json --prefix "$HOME/.local"
 popd || return
 
 # plugin directory
