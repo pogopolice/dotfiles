9b9ba9a59ff9bb6522ef4f6ecf31388131988e1c
pogopolice <pogopolice@gmail.com>
2020-09-07 13:00:11 -0400

    set the local library location with cpanm

--- neovim_base.sh
+++ neovim_base.sh
@@ -30,6 +30,7 @@ sudo apt install -y nodejs
 
 # perl
 sudo apt install -y perl cpanminus
+cpanm --local-lib=~/perl5 local::lib && eval "$(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)"
 cpanm Neovim::Ext
 
 # aws cli 2
