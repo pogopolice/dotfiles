9c492741d9ec2c0a7ee74e5b5028e6253a74e384
pogopolice <pogopolice@gmail.com>
2020-09-08 14:55:17 -0400

    reduce the comment footprint

--- coc_setup.sh
+++ coc_setup.sh
@@ -1,57 +1,25 @@
 #!/bin/bash -
-#===============================================================================
-#
-#          FILE: coc_setup.sh
-#
-#         USAGE: ./coc_setup.sh
-#
-#   DESCRIPTION: 
-#
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 08/19/2020 02:22:37 PM
-#      REVISION:  ---
-#===============================================================================
 
-set -o nounset                                  # Treat unset variables as an error
 
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  install_plugin
-#   DESCRIPTION:  Install COC using vim's native package manager
-#-------------------------------------------------------------------------------
-
+# Install COC using vim's native package manager
 install_plugin ()
 {
   mkdir -p ~/.local/share/nvim/site/pack/pogopolice/start
   pushd ~/.local/share/nvim/site/pack/pogopolice/start || return
   curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz|tar xzfv -
-  git clone https://github.com/WolfgangMehner/bash-support.git
-  git clone https://github.com/vim-airline/vim-airline.git
-  git clone https://github.com/tpope/vim-fugitive.git
   popd || return
 
 }  # ----------  end of function install_plugin  ----------
 
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  copy_config
-#   DESCRIPTION: copy the coc config file to the nvim plug conig directory
-#-------------------------------------------------------------------------------
 
+# Copy the coc config file to the nvim plug conig directory
 copy_config ()
 {
   cp "$PWD/coc.vim" "$HOME/.config/nvim/plug-config/coc.vim"
 }  # ----------  end of function copy_config  ----------
 
 
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  source_config
-#   DESCRIPTION:  add a source call to the coc config in the neovim config
-#-------------------------------------------------------------------------------
-
+# Add a source call to the coc config in the neovim config
 source_config ()
 {
 printf "%s\\n" "
@@ -61,12 +29,7 @@ source \$HOME/.config/nvim/plug-config/coc.vim
 }  # ----------  end of function source_config  ----------
 
 
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  main
-#   DESCRIPTION: execute these functions in sequence
-#-------------------------------------------------------------------------------
-
-
+# Execute these functions in sequence
 main ()
 {
   install_plugin
