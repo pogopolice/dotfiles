adb10ad4297b12a292d7e6cbbc2590b0fec8d953
pogopolice <pogopolice@gmail.com>
2020-09-08 14:54:50 -0400

    reduce the comment footprint

--- coc_settings.sh
+++ coc_settings.sh
@@ -1,29 +1,7 @@
 #!/bin/bash -
-#===============================================================================
-#
-#          FILE: coc_settings.sh
-#
-#         USAGE: ./coc_settings.sh
-#
-#   DESCRIPTION: 
-#
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
-#        AUTHOR: YOUR NAME (), 
-#  ORGANIZATION: 
-#       CREATED: 08/21/2020 03:42:24 PM
-#      REVISION:  ---
-#===============================================================================
-
-set -o nounset                                  # Treat unset variables as an error
 
 
-#---  FUNCTION  ----------------------------------------------------------------
-#          NAME:  copy_coc_settings
-#   DESCRIPTION: copy the coc settings file into the nvim config directory 
-#-------------------------------------------------------------------------------
+# Copy the coc settings file into the nvim config directory 
 copy_coc_settings ()
 {
 	cp "$PWD/coc-settings.json" "$HOME/.config/nvim"
